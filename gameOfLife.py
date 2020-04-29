71a578c79992a3a1e42e9a5a1808d2a5fcbfd5a8
rbnfr <ruben.f.alcaraz@gmail.com>
2020-04-29 16:41:56 +0200

    Change cell type using mouse

--- gameOfLife.py
+++ gameOfLife.py
@@ -61,7 +61,12 @@ while not endGame:
             posX,posY = pygame.mouse.get_pos()
             cellX = int(np.floor(posX / dimCellWidth))
             cellY = int(np.floor(posY / dimCellHeight))
-            newGameState[cellX, cellY] = not mouseClick[2]
+            if newGameState[cellX, cellY] == 0:
+                newGameState[cellX, cellY] = 1
+            elif newGameState[cellX, cellY] == 1:
+                newGameState[cellX, cellY] = 2
+            elif newGameState[cellX, cellY] == 2:
+                newGameState[cellX, cellY] = 0
 
         if mouseClick[1]:
             pauseExecution = not pauseExecution
